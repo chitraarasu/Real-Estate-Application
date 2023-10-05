5ddd889d445b024ce48438a2ca9d2d8a3ceb41df
Chitraarasu <98887175+chitraarasu-kirshi@users.noreply.github.com>
2023-10-05 11:28:54 +0530

    build setup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:real_estate/screens/dashboard/dashboard.dart';
@@ -12,7 +13,8 @@ import 'firebase_options.dart';
 import 'screens/onboarding/onboarding_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final binding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: binding);
   await GetStorage.init();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -23,6 +25,9 @@ void main() async {
       DeviceOrientation.portraitUp,
     ],
   );
+  Future.delayed(const Duration(seconds: 1), () {
+    FlutterNativeSplash.remove();
+  });
   runApp(MyApp());
 }
 
